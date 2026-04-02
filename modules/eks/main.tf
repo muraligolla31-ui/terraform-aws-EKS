6dc5df9e5cca7869b7ae7c6bf71c73989cd84508
Admin <muraligolla31@gmail.com>
2026-04-02 22:06:55 +0530

    Replace EKS module with native AWS resources and add .gitignore

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -22,29 +22,143 @@ module "vpc" {
   }
 }
 
+# Security Group for Node Group
+resource "aws_security_group" "node_sg" {
+  name        = "${var.cluster_name}-node-sg"
+  description = "Security group for EKS nodes"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name        = "${var.cluster_name}-node-sg"
+    Environment = "dev"
+  }
+}
+
+# Allow nodes to communicate with cluster
+resource "aws_security_group_rule" "node_ingress_cluster" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  source_security_group_id = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+  security_group_id        = aws_security_group.node_sg.id
+}
+
+# Allow nodes to communicate with each other
+resource "aws_security_group_rule" "node_ingress_self" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  self              = true
+  security_group_id = aws_security_group.node_sg.id
+}
+
+# Allow outbound traffic
+resource "aws_security_group_rule" "node_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.node_sg.id
+}
+
+# IAM Role for EKS Cluster
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "${var.cluster_name}-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
 # EKS Cluster
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "20.0"
+resource "aws_eks_cluster" "main" {
+  name     = var.cluster_name
+  version  = "1.29"
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
-  cluster_name    = var.cluster_name
-  cluster_version = "1.29"
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  enable_irsa = true
-
-  eks_managed_node_groups = {
-    default = {
-      instance_types = ["t3.medium"]
-      min_size       = 1
-      max_size       = 2
-      desired_size   = 1
-    }
+  vpc_config {
+    subnet_ids = concat(module.vpc.public_subnets, module.vpc.private_subnets)
   }
 
   tags = {
     Environment = "dev"
   }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
+}
+
+# IAM Role for EKS Node Group
+resource "aws_iam_role" "eks_node_role" {
+  name = "${var.cluster_name}-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_container_registry_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+# EKS Managed Node Group
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.cluster_name}-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = module.vpc.private_subnets
+
+  instance_types = ["t3.medium"]
+
+  scaling_config {
+    min_size     = 1
+    max_size     = 2
+    desired_size = 1
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.eks_container_registry_policy,
+  ]
 }
--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,7 +1,19 @@
 output "cluster_name" {
-  value = module.eks.cluster_name
+  value       = aws_eks_cluster.main.name
+  description = "Name of the EKS cluster"
 }
 
 output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+  value       = aws_eks_cluster.main.endpoint
+  description = "Endpoint for EKS control plane"
+}
+
+output "cluster_version" {
+  value       = aws_eks_cluster.main.version
+  description = "The Kubernetes server version for the cluster"
+}
+
+output "cluster_security_group_id" {
+  value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+  description = "Security group ID attached to the EKS cluster"
 }
