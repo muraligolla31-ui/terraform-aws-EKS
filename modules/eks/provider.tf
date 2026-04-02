2f6134b582730041ee5cfec43edb235bc5c710d6
muraligolla31-ui <muraligolla31@gmail.com>
2026-04-02 21:37:07 +0530

    Update AWS provider version constraint in provider.tf

--- modules/eks/provider.tf
+++ modules/eks/provider.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 1.3"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "~> 5.40"  # Latest compatible version
     }
   }
 }
